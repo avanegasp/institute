deaab12df38cc61fafffea7f82873874983d3004
Angie <arco527@gmail.com>
2019-02-05 18:47:21 -0500

    Create controllers

--- app/models/student.rb
+++ app/models/student.rb
@@ -1,4 +1,7 @@
 class Student < ApplicationRecord
   belongs_to :course
   belongs_to :school
+  validates :name, uniqueness: true
+  validates :email, uniqueness: true
+  validates :name, presence: true
 end
