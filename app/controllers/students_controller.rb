1acfc27358ad3d41b69d19c1a0e6479914aedd7b
Angie <arco527@gmail.com>
2019-02-14 23:07:04 -0500

    Create new student

--- app/controllers/students_controller.rb
+++ app/controllers/students_controller.rb
@@ -2,18 +2,16 @@ class StudentsController < ApplicationController
 
   def index
     @students = Student.all
-  end
-
-  def new
     @student = Student.new
   end
 
+  def new
+  end
+
   def create
-    @student = Student.new(student_params)
+    @student = Student.create(student_params)
     byebug
-    if @student.save
-        redirect_to "/"
-    end
+    @students = Student.all
   end
 
   def show
@@ -22,6 +20,6 @@ class StudentsController < ApplicationController
 
   private
   def student_params
-    params.require(:student).permit(:name, :last_name, :address, :neighborhood, :email, :school_id, :course_id)
+    params.require(:student).permit(:name, :last_name, :address, :neighborhood, :email)
   end
 end
--- app/models/student.rb
+++ app/models/student.rb
@@ -1,7 +1,4 @@
 class Student < ApplicationRecord
   belongs_to :course
   belongs_to :school
-  validates :name, uniqueness: true
-  validates :email, uniqueness: true
-  validates :name, presence: true
 end
